5adb8e3086ea87fc0fa2d26a79abf8b5f7dc7147
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2023-01-13 05:58:00 -0800

    Test load_dotenv to read .env values

--- src/test.py
+++ src/test.py
@@ -1,44 +1,9 @@
-import pandas as pd
+import os
 
-df = pd.DataFrame(
-    {
-        "a": [
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-        ]
-    }
-)
+from dotenv import load_dotenv
+
+local_env = "../.env_example"
+load_dotenv(local_env)
+
+server = os.getenv("SERVER")
+db = os.getenv("POSTGRES_DB")
